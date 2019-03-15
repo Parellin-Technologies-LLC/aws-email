75f7b3894e76cf088722e4b9a7a8b730209df05f
iskore <nicksoggin@gmail.com>
2019-03-15 16:46:31 -0400

    Auto stash before rebase of "origin/master"

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import { AmplifyPlugin } from 'aws-amplify-vue';
+import awsmobile from './aws-exports';
+
+Amplify.configure( awsmobile );
+
+Vue.use( AmplifyPlugin, AmplifyModules );
+
+// It's important that you instantiate the Vue instance after calling Vue.use!
+
+new Vue( {
+	render: h => h( App )
+} ).$mount( '#app' );
