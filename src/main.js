e1ff7ecee18fc29eaec599bdc6e779c2935fc897
iskore <nicksoggin@gmail.com>
2019-03-16 01:51:56 -0400

    added vuetify

--- src/main.js
+++ src/main.js
@@ -1,17 +1,19 @@
 import Vue from 'vue';
-import App from './App.vue';
+
+import 'babel-polyfill';
+import './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
-// import Amplify, * as AmplifyModules from 'aws-amplify';
-// import { AmplifyPlugin } from 'aws-amplify-vue';
-// import awsmobile from './aws-exports';
-//
-// Amplify.configure( awsmobile );
-//
-// Vue.use( AmplifyPlugin, AmplifyModules );
+import Amplify, * as AmplifyModules from 'aws-amplify';
+import { AmplifyPlugin } from 'aws-amplify-vue';
+import awsmobile from './aws-exports';
 
-// It's important that you instantiate the Vue instance after calling Vue.use!
+Amplify.configure( awsmobile );
+
+Vue.use( AmplifyPlugin, AmplifyModules );
+
+import App from './App.vue';
 
 new Vue( {
 	render: h => h( App )
