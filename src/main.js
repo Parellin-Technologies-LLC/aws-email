5b9e7c1429f51914f01d7573df9e48034d4fad44
Nick Soggin <nicksoggin@gmail.com>
2019-03-16 00:01:40 -0400

    build test

--- src/main.js
+++ src/main.js
@@ -3,13 +3,13 @@ import App from './App.vue';
 
 Vue.config.productionTip = false;
 
-import Amplify, * as AmplifyModules from 'aws-amplify';
-import { AmplifyPlugin } from 'aws-amplify-vue';
-import awsmobile from './aws-exports';
-
-Amplify.configure( awsmobile );
-
-Vue.use( AmplifyPlugin, AmplifyModules );
+// import Amplify, * as AmplifyModules from 'aws-amplify';
+// import { AmplifyPlugin } from 'aws-amplify-vue';
+// import awsmobile from './aws-exports';
+//
+// Amplify.configure( awsmobile );
+//
+// Vue.use( AmplifyPlugin, AmplifyModules );
 
 // It's important that you instantiate the Vue instance after calling Vue.use!
 
