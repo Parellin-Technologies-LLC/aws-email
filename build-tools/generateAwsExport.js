d5b989d700d2a71ab2a38797b64b05a6675d14b8
iskore <nicksoggin@gmail.com>
2019-04-30 14:12:22 -0400

    update env config

--- build-tools/generateAwsExport.js
+++ build-tools/generateAwsExport.js
@@ -8,18 +8,18 @@ const
 	{ writeFileSync } = require( 'fs' ),
 	{ join }          = require( 'path' );
 
-const awsmobile = {
-	aws_project_region: process.env.project_region || '',
-	aws_cognito_identity_pool_id: process.env.cognito_identity_pool_id || '',
-	aws_cognito_region: process.env.cognito_region || '',
-	aws_user_pools_id: process.env.user_pools_id || '',
-	aws_user_pools_web_client_id: process.env.user_pools_web_client_id || '',
-	aws_content_delivery_bucket: process.env.content_delivery_bucket || '',
-	aws_content_delivery_bucket_region: process.env.content_delivery_bucket_region || '',
-	aws_content_delivery_url: process.env.content_delivery_url || ''
-};
+// const awsmobile = {
+// aws_project_region: process.env.project_region || '',
+// aws_cognito_identity_pool_id: process.env.cognito_identity_pool_id || '',
+// aws_cognito_region: process.env.cognito_region || '',
+// aws_user_pools_id: process.env.user_pools_id || '',
+// aws_user_pools_web_client_id: process.env.user_pools_web_client_id || '',
+// aws_content_delivery_bucket: process.env.content_delivery_bucket || '',
+// aws_content_delivery_bucket_region: process.env.content_delivery_bucket_region || '',
+// aws_content_delivery_url: process.env.content_delivery_url || ''
+// };
 
 writeFileSync(
 	join( process.cwd(), 'src', 'aws-exports.js' ),
-	`export default ${ JSON.stringify( awsmobile ) }`
+	`export default ${ JSON.stringify( process.env.project_aws_exports ) }`
 );
